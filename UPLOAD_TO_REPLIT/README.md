980e563c1201c0b1f99eee43bfd8146fa3eba685
Abba Yosua <abbasiagian@gmail.com>
2023-06-16 23:11:12 +0700

    update

---
+++ UPLOAD_TO_REPLIT/README.md
@@ -0,0 +1,48 @@
+## Registration Form
+
+This is a simple registration form using HTML, Bootstrap, and JavaScript.
+
+### Getting Started
+
+1. Clone this repository: `git clone https://github.com/example/example.git`.
+2. Open `index.html` in your browser or run it on your local server.
+
+### Prerequisites
+
+- Any web browser (Google Chrome, Mozilla Firefox, Safari, etc.)
+- Basic knowledge in HTML and JavaScript
+
+### Built With
+
+- HTML
+- Bootstrap 4.5.2
+- JavaScript
+
+### Usage
+
+This form can be used for event registration, membership registration, or any other similar purposes.
+
+### Features
+
+- Email registration
+- Full name registration
+- Date of birth registration
+- City of origin registration
+- Phone number registration
+- Church position registration (drop-down menu)
+- T-shirt size registration (drop-down menu)
+- Blazer size registration (drop-down menu)
+- Profile picture upload (with preview)
+- Quotes/words registration
+
+### Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+### Authors
+
+- [Abba](https://github.com/abbayosua) - Creator
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/example/example/blob/master/LICENSE) file for details.
--- public/scriptindex.js
+++ public/scriptindex.js
@@ -1,49 +1,50 @@
 
 document.querySelector('#registration-form').addEventListener('submit', function(event) {
-    var phoneNumberInput = document.querySelector('#phone_number');
-      var phoneNumberValue = phoneNumberInput.value;
-      if (phoneNumberValue.startsWith('08')) {
-        phoneNumberInput.value = '628' + phoneNumberValue.slice(2).replace(/[^0-9]/g, '');
-      }
-  });
-  window.onload = (async function () {
-    const compressor = new window.Compress();
-    console.log(compressor)
-    const browserSupportsExifOrientation = () => {
-      return new Promise((resolve) => Modernizr.on("exiforientation", resolve));
-    };
+  var phoneNumberInput = document.querySelector('#phone_number');
+  var profilePicture = document.querySelector('#profile_picture');
+  var phoneNumberValue = phoneNumberInput.value;
+  if (phoneNumberValue.startsWith('08')) {
+    phoneNumberInput.value = '628' + phoneNumberValue.slice(2).replace(/[^0-9]/g, '');
+  }
+  profilePicture.value = profilePicture.value.replace(/^data:image\/(png|jpg|jpeg);base64,/, "")
+});
+// window.onload = (async function() {
+//   const compressor = new window.Compress();
+//   console.log(compressor)
+//   const browserSupportsExifOrientation = () => {
+//     return new Promise((resolve) => Modernizr.on("exiforientation", resolve));
+//   };
 
-    // Only rotate if browser does not support exit orientation.
-    const shouldRotate = async () => {
-      const supported = await browserSupportsExifOrientation();
-      return !supported;
-    };
-    const rotate = await shouldRotate();
-    console.log({ rotate });
+//   // Only rotate if browser does not support exit orientation.
+//   const shouldRotate = async () => {
+//     const supported = await browserSupportsExifOrientation();
+//     return !supported;
+//   };
+//   const rotate = await shouldRotate();
+//   console.log({ rotate });
 
-    const upload = document.getElementById("set_file");
-    const preview = document.getElementById("preview");
-    const profilePicture = document.getElementById("profile_picture")
-    upload.addEventListener(
-      "change",
-      async function (evt) {
-        const files = [...evt.target.files];
-        const results = await compressor.compress(files, {
-          size: 4,
-          quality: 0.75,
-          // rotate,
-        });
-        console.log(results);
-        const output = results[0];
-        const file = Compress.convertBase64ToFile(output.data, output.ext);
-        console.log(file);
-        preview.src = output.prefix + output.data;
-        profilePicture.value = output.data;
-      },
-      false
-    );
-  })();
-  
+//   const upload = document.getElementById("set_file");
+//   const preview = document.getElementById("preview");
+//   const profilePicture = document.getElementById("profile_picture")
+//   upload.addEventListener(
+//     "change",
+//     async function(evt) {
+//       const files = [...evt.target.files];
+//       const results = await compressor.compress(files, {
+//         size: 4,
+//         quality: 0.75,
+//         // rotate,
+//       });
+//       console.log(results);
+//       const output = results[0];
+//       const file = Compress.convertBase64ToFile(output.data, output.ext);
+//       console.log(file);
+//       preview.src = output.prefix + output.data;
+//       profilePicture.value = output.data;
+//     },
+//     false
+//   );
+// })();
+
 // document.getElementById('registration-form').addEventListener('submit', function(event) {
 //   });
-  
